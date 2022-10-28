6fdd83f8da9259f98aa3924f29bd6fa96f0f2150
Bagas <mbagas221@gmail.com>
2022-10-28 23:14:35 +0700

    update

--- Front/src/pages/DetailRest.js
+++ Front/src/pages/DetailRest.js
@@ -1,90 +1,13 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Foods } from "../data/Foods";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import { Alert } from "react-bootstrap";
-import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
-import { useMutation } from "react-query";
 import { API } from "../config/api";
-import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
+import { useParams } from 'react-router-dom';
 
-function Detail({ addItem }) {
-  const navigate = useNavigate();
-
-  const [state, dispatch] = useContext(UserContext);
-
-  const [message, setMessage] = useState(null);
-
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-    fullName: "",
-    gender: "",
-    phone: "",
-    role: "",
-  });
-
-  const { fullName, email, password, gender, phone, role } = form;
-
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmitRegist = useMutation(async (e) => {
-    try {
-      e.preventDefault();
-
-      const response = await API.post("/register", form);
-
-      const alert = <Alert variant="success">Berhasil register!</Alert>;
-
-      setMessage(alert);
-    } catch (e) {
-      console.log(e);
-      const alert = <Alert variant="danger">Register gagal!</Alert>;
-
-      setMessage(alert);
-    }
-  });
-
-  const handleSubmitLogin = useMutation(async (e) => {
-    try {
-      e.preventDefault();
-
-      const data = await API.post("/login", form);
-
-      const alert = <Alert variant="success">Login berhasil!</Alert>;
-
-      setMessage(alert);
-
-      let payload = data.data.data;
-
-      dispatch({
-        type: "LOGIN_SUCCESS",
-        payload,
-      });
-
-      navigate("/");
-
-      console.log("isi payload", payload);
-      console.log("ini data login", data);
-    } catch (error) {
-      console.log(error);
-      const alert = <Alert variant="danger">Email / password salah!</Alert>;
-
-      setMessage(alert);
-    }
-  });
-
+function Detail() {
   const showToastMessage = () => {
     toast.success("Sukses menambahkan ke keranjang!", {
       position: "bottom-right",
@@ -101,14 +24,6 @@ function Detail({ addItem }) {
     document.title = "Geprek Bensu's Menu";
   }, []);
 
-  const [show, setShow] = useState(false);
-  const [show1, setShow1] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const handleClose1 = () => setShow1(false);
-  const handleShow1 = () => setShow1(true);
-
   let { id } = useParams();
   let { data: products } = useQuery("productsCache", async () => {
     const response = await API.get("/products");
@@ -118,169 +33,6 @@ function Detail({ addItem }) {
 
   return (
     <div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Body>
-          {message && message}
-          <Form className="p-3" onSubmit={(e) => handleSubmitRegist.mutate(e)}>
-            <h3 className="mb-4 fw-bold" style={{ color: "#FFC700" }}>
-              Register
-            </h3>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="email"
-                placeholder="Email"
-                style={{ backgroundColor: "#F4F4F4" }}
-                onChange={handleChange}
-                name="email"
-                value={email}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                style={{ backgroundColor: "#F4F4F4" }}
-                onChange={handleChange}
-                name="password"
-                value={password}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                placeholder="Full Name"
-                style={{ backgroundColor: "#F4F4F4" }}
-                value={fullName}
-                onChange={handleChange}
-                name="fullName"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" style={{ backgroundColor: "#F4F4F4" }}>
-              <Form.Select
-                style={{ backgroundColor: "#F4F4F4" }}
-                value={gender}
-                onChange={handleChange}
-                name="gender"
-              >
-                <option hidden>Gender</option>
-                <option>Male</option>
-                <option>Female</option>
-              </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="number"
-                placeholder="Phone"
-                style={{ backgroundColor: "#F4F4F4" }}
-                onChange={handleChange}
-                name="phone"
-                value={phone}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Select
-                style={{ backgroundColor: "#F4F4F4" }}
-                value={role}
-                onChange={handleChange}
-                name="role"
-              >
-                <option>As User</option>
-                <option>As Partner</option>
-              </Form.Select>
-            </Form.Group>
-            <Button
-              type="submit"
-              style={{ width: "100%", background: "#433434", border: "none" }}
-            >
-              Register
-            </Button>
-          </Form>
-          <div className="d-flex justify-content-center mx-auto">
-            <p>
-              Already have an account ? Click{" "}
-              <button
-                onClick={() => {
-                  handleShow1();
-                  handleClose();
-                }}
-                style={{ border: "none", backgroundColor: "white" }}
-                className="p-0"
-              >
-                here
-              </button>
-            </p>
-          </div>
-        </Modal.Body>
-      </Modal>
-
-      <Modal show={show1} onHide={handleClose1}>
-        <Modal.Body>
-          <Form className="p-3" onSubmit={(e) => handleSubmitLogin.mutate(e)}>
-            <h3 className="mb-4 fw-bold" style={{ color: "#FFC700" }}>
-              Login
-            </h3>
-            <Form.Group className="mb-3">
-              <Form.Control
-                id="email"
-                type="email"
-                placeholder="Email"
-                style={{ backgroundColor: "#F4F4F4" }}
-                value={email}
-                name="email"
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                id="password"
-                type="password"
-                placeholder="Password"
-                style={{ backgroundColor: "#F4F4F4" }}
-                value={password}
-                name="password"
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Select
-                style={{ backgroundColor: "#F4F4F4" }}
-                value={role}
-                name="role"
-                onChange={handleChange}
-              >
-                <option hidden>Role</option>
-                <option value="user">As User</option>
-                <option value="admin">As Partner</option>
-              </Form.Select>
-            </Form.Group>
-            <Button
-              type="submit"
-              style={{ width: "100%", background: "#433434", border: "none" }}
-              onClick={() => {
-                handleClose1();
-              }}
-            >
-              Login
-            </Button>
-          </Form>
-          <div className="d-flex justify-content-center mx-auto">
-            <p>
-              Don't have an account ? Click{" "}
-              <button
-                onClick={() => {
-                  handleShow();
-                  handleClose1();
-                }}
-                style={{ border: "none", backgroundColor: "white" }}
-                className="p-0"
-              >
-                here
-              </button>
-            </p>
-          </div>
-        </Modal.Body>
-      </Modal>
-
       <ToastContainer
         position="bottom-right"
         autoClose={2000}
@@ -299,7 +51,6 @@ function Detail({ addItem }) {
       >
         <div className="m-5" style={{ width: "70%" }}>
           <h2 className="fw-bold mb-4">Menu</h2>
-          {state.isLogin ? (
             <div className="d-flex justify-content-evenly flex-wrap">
               {products?.map((p) => (
                 <Card
@@ -320,7 +71,6 @@ function Detail({ addItem }) {
                       }}
                       className="py-1 text-dark"
                       onClick={() => {
-                        addItem(p);
                         showToastMessage();
                       }}
                     >
@@ -330,37 +80,6 @@ function Detail({ addItem }) {
                 </Card>
               ))}
             </div>
-          ) : (
-            <div className="d-flex justify-content-evenly flex-wrap">
-              {Foods.map((p) => (
-                <Card
-                  style={{ width: "14rem" }}
-                  className="p-2 mb-3"
-                  key={p.id}
-                >
-                  <Card.Img variant="top" src={p.image} />
-                  <Card.Body className="py-3 px-1">
-                    <Card.Title className="fs-6">{p.name}</Card.Title>
-                    <Card.Text className="text-danger">{p.price1}</Card.Text>
-                    <Button
-                      style={{
-                        marginBottom: "-10px",
-                        width: "100%",
-                        backgroundColor: "#FFC700",
-                        border: "none",
-                      }}
-                      className="py-1 text-dark"
-                      onClick={() => {
-                        handleShow();
-                      }}
-                    >
-                      Order
-                    </Button>
-                  </Card.Body>
-                </Card>
-              ))}
-            </div>
-          )}
         </div>
       </div>
     </div>
--- Front/src/pages/LandingPage.js
+++ Front/src/pages/LandingPage.js
@@ -1,12 +1,21 @@
 import Pizza from "../assets/pizza.png";
 import Line from "../assets/garis.png";
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { API } from "../config/api";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API, setAuthToken } from "../config/api";
 import { useQuery } from "react-query";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { Alert } from "react-bootstrap";
+import { useMutation } from "react-query";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import Button from "react-bootstrap/Button";
 
 function Page() {
   const navigate = useNavigate();
+
+  const [state, dispatch] = useContext(UserContext);
 
   useEffect(() => {
     document.title = "Ways Food";
@@ -17,8 +26,248 @@ function Page() {
     return response.data.data;
   });
 
+  const [message, setMessage] = useState(null);
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    fullName: "",
+    gender: "",
+    phone: "",
+    role: "",
+  });
+
+  const { fullName, email, password, gender, phone, role } = form;
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmitRegist = useMutation(async (e) => {
+    try {
+      e.preventDefault();
+
+      const alert = <Alert variant="success">Berhasil register!</Alert>;
+
+      setMessage(alert);
+    } catch (e) {
+      console.log(e);
+      const alert = <Alert variant="danger">Register gagal!</Alert>;
+
+      setMessage(alert);
+    }
+  
+  });
+
+  const handleSubmitLogin = useMutation(async (e) => {
+    try {
+      e.preventDefault();
+
+      const data = await API.post("/login", form);
+
+      const alert = <Alert variant="success">Login berhasil!</Alert>;
+
+      setMessage(alert);
+
+      let payload = data.data.data;
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload,
+      });
+
+      navigate("/");
+
+      console.log("isi payload", payload);
+      console.log("ini data login", data);
+    } catch (error) {
+      console.log(error);
+      const alert = <Alert variant="danger">Email / password salah!</Alert>;
+
+      setMessage(alert);
+    }
+  });
+
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
   return (
     <div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          {message && message}
+          <Form className="p-3" onSubmit={(e) => handleSubmitRegist.mutate(e)}>
+            <h3 className="mb-4 fw-bold" style={{ color: "#FFC700" }}>
+              Register
+            </h3>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                style={{ backgroundColor: "#F4F4F4" }}
+                onChange={handleChange}
+                name="email"
+                value={email}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                style={{ backgroundColor: "#F4F4F4" }}
+                onChange={handleChange}
+                name="password"
+                value={password}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="text"
+                placeholder="Full Name"
+                style={{ backgroundColor: "#F4F4F4" }}
+                value={fullName}
+                onChange={handleChange}
+                name="fullName"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" style={{ backgroundColor: "#F4F4F4" }}>
+              <Form.Select
+                style={{ backgroundColor: "#F4F4F4" }}
+                value={gender}
+                onChange={handleChange}
+                name="gender"
+              >
+                <option hidden>Gender</option>
+                <option>Male</option>
+                <option>Female</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="number"
+                placeholder="Phone"
+                style={{ backgroundColor: "#F4F4F4" }}
+                onChange={handleChange}
+                name="phone"
+                value={phone}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Select
+                style={{ backgroundColor: "#F4F4F4" }}
+                value={role}
+                onChange={handleChange}
+                name="role"
+              >
+                <option>As User</option>
+                <option>As Partner</option>
+              </Form.Select>
+            </Form.Group>
+            <Button
+              type="submit"
+              style={{ width: "100%", background: "#433434", border: "none" }}
+            >
+              Register
+            </Button>
+          </Form>
+          <div className="d-flex justify-content-center mx-auto">
+            <p>
+              Already have an account ? Click{" "}
+              <button
+                onClick={() => {
+                  handleShow1();
+                  handleClose();
+                }}
+                style={{ border: "none", backgroundColor: "white" }}
+                className="p-0"
+              >
+                here
+              </button>
+            </p>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <Modal show={show1} onHide={handleClose1}>
+        <Modal.Body>
+          <Form className="p-3" onSubmit={(e) => handleSubmitLogin.mutate(e)}>
+            <h3 className="mb-4 fw-bold" style={{ color: "#FFC700" }}>
+              Login
+            </h3>
+            <Form.Group className="mb-3">
+              <Form.Control
+                id="email"
+                type="email"
+                placeholder="Email"
+                style={{ backgroundColor: "#F4F4F4" }}
+                value={email}
+                name="email"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                id="password"
+                type="password"
+                placeholder="Password"
+                style={{ backgroundColor: "#F4F4F4" }}
+                value={password}
+                name="password"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Select
+                style={{ backgroundColor: "#F4F4F4" }}
+                value={role}
+                name="role"
+                onChange={handleChange}
+              >
+                <option hidden>Role</option>
+                <option value="user">As User</option>
+                <option value="admin">As Partner</option>
+              </Form.Select>
+            </Form.Group>
+            <Button
+              type="submit"
+              style={{ width: "100%", background: "#433434", border: "none" }}
+              onClick={() => {
+                handleClose1();
+              }}
+            >
+              Login
+            </Button>
+          </Form>
+          <div className="d-flex justify-content-center mx-auto">
+            <p>
+              Don't have an account ? Click{" "}
+              <button
+                onClick={() => {
+                  handleShow();
+                  handleClose1();
+                }}
+                style={{ border: "none", backgroundColor: "white" }}
+                className="p-0"
+              >
+                here
+              </button>
+            </p>
+          </div>
+        </Modal.Body>
+      </Modal>
+
       <div
         className="d-flex justify-content-center align-items-center mx-auto"
         style={{ backgroundColor: "#FFC700", marginBottom: "60px" }}
@@ -43,49 +292,98 @@ function Page() {
       <div className="mx-auto" style={{ width: "80%" }}>
         <h3 className="fw-bold">Popular Restaurant</h3>
       </div>
-      <div
-        className="my-4 mx-auto"
-        style={{
-          width: "85%",
-          overflowY: "scroll",
-          overflowX: "hidden",
-          height: "400px",
-        }}
-      >
-        <div className="d-flex flex-wrap" style={{ justifyContent: "center" }}>
-          {admins?.map((p) => (
-            
-            <div
-              className="my-4 p-3 rounded mx-3"
-              style={{
-                backgroundColor: "white",
-                width: "20%",
-                cursor: "pointer",
-              }}
-              key={p.id}
-              onClick={() => {
-                navigate(`/detail-restaurant/${p.id}`);
-              }}
-            >
-              {p.image == "https://localhost:5000/uploads/" ? (
-                <img src={p.image} alt="" />
-              ) : (
-                <img
-                  src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
-                  alt=""
-                  width="60px"
-                  height="60px"
-                  style={{ borderRadius: "50%" }}
-                />
-              )}
-              <span className="fw-bold ms-3" style={{ fontSize: "18px" }}>
-                {p.fullName}
-              </span>
-            </div>
 
-          ))}
+      {state.isLogin ? (
+        <div
+          className="my-4 mx-auto"
+          style={{
+            width: "85%",
+            overflowY: "scroll",
+            overflowX: "hidden",
+            height: "400px",
+          }}
+        >
+          <div
+            className="d-flex flex-wrap"
+            style={{ justifyContent: "center" }}
+          >
+            {admins?.map((p) => (
+              <div
+                className="my-4 p-3 rounded mx-3"
+                style={{
+                  backgroundColor: "white",
+                  width: "20%",
+                  cursor: "pointer",
+                }}
+                key={p.id}
+                onClick={() => {
+                  navigate(`/detail-restaurant/${p.id}`);
+                }}
+              >
+                {p.image === "https://localhost:5000/uploads/" ? (
+                  <img src={p.image} alt="" />
+                ) : (
+                  <img
+                    src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                    alt=""
+                    width="60px"
+                    height="60px"
+                    style={{ borderRadius: "50%" }}
+                  />
+                )}
+                <span className="fw-bold ms-3" style={{ fontSize: "18px" }}>
+                  {p.fullName}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div
+          className="my-4 mx-auto"
+          style={{
+            width: "85%",
+            overflowY: "scroll",
+            overflowX: "hidden",
+            height: "400px",
+          }}
+        >
+          <div
+            className="d-flex flex-wrap"
+            style={{ justifyContent: "center" }}
+          >
+            {admins?.map((p) => (
+              <div
+                className="my-4 p-3 rounded mx-3"
+                style={{
+                  backgroundColor: "white",
+                  width: "20%",
+                  cursor: "pointer",
+                }}
+                key={p.id}
+                onClick={() => {
+                  handleShow();
+                }}
+              >
+                {p.image === "https://localhost:5000/uploads/" ? (
+                  <img src={p.image} alt="" />
+                ) : (
+                  <img
+                    src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                    alt=""
+                    width="60px"
+                    height="60px"
+                    style={{ borderRadius: "50%" }}
+                  />
+                )}
+                <span className="fw-bold ms-3" style={{ fontSize: "18px" }}>
+                  {p.fullName}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
 
       <div className="my-4 mx-auto" style={{ width: "75%" }}></div>
     </div>
